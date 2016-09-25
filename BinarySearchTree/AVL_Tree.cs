e926fd3a60e8469746d2bb95f8aa83b72038a3c7
jack torres <jacintotorres92@outlook.com>
2016-09-25 03:10:39 -0400

    Added Deletion Functions Added Menu Tested Balance

--- BinarySearchTree/AVL_Tree.cs
+++ BinarySearchTree/AVL_Tree.cs
@@ -13,9 +13,7 @@ namespace BinarySearchTree
         {
             Random randNum = new Random();
             for (int i = 0; i < amount; i++)
-            {
                 Insert(randNum.Next(0, 100));
-            }
         }
 
         // Rotate Left - Used in RR, RL, and LR cases
@@ -27,8 +25,8 @@ namespace BinarySearchTree
             newN.left = N;
 
             // Set heights
-            setHeight(ref N);
-            setHeight(ref newN);
+            SetHeight(ref N);           // set height of new left
+            SetHeight(ref newN);        // set height of new root
 
             // Set N to New N
             N = newN;
@@ -43,25 +41,60 @@ namespace BinarySearchTree
             newN.right = N;
 
             // Set heights
-            setHeight(ref N);
-            setHeight(ref newN);
+            SetHeight(ref N);
+            SetHeight(ref newN);
 
             // Set N to New N 
             N = newN;
         }
 
         // Sets the height equal to 1 + max height of child nodes
-        private void setHeight(ref Node N)
+        private void SetHeight(ref Node N)
         {
-            if (N == null)
-                N.height = 0;
-            N.height = 1 + Math.Max(height(N.left), height(N.right));
+            if (N != null)
+                N.height = 1 + Math.Max(Height(N.left), Height(N.right));
         }
 
         // Gets the height of the provided Node
-        private int height(Node N)
+        private int Height(Node N)
         {
             return (N == null) ? 0 : N.height;
+        }
+
+        // Balance The tree from the provided Node 
+        private void Balance(ref Node N)
+        {
+            if (N != null)
+            { 
+                int balance = Height(N.left) - Height(N.right);
+                if (balance > 1)
+                {
+                    // LL
+                    if (Height(N.left.left) >= Height(N.left.right))
+                        RotateRight(ref N);
+                    // LR
+                    else
+                    {
+                        RotateLeft(ref N.left);
+                        RotateRight(ref N);
+                    }
+                }
+                else if (balance < -1)
+                {
+                    // RR
+                    if (Height(N.right.right) >= Height(N.right.left))
+                        RotateLeft(ref N);
+                    // RL
+                    else
+                    {
+                        RotateRight(ref N.right);
+                        RotateLeft(ref N);
+                    }
+                }
+                else
+                    // Set the new height
+                    SetHeight(ref N);
+            }
         }
 
         // Standard Insert - Uses InsertRecursive
@@ -77,7 +110,7 @@ namespace BinarySearchTree
             if (N == null)
                 N = new Node(theValue);
 
-            // if new balue less than current node, go to left node
+            // if new value less than current node, go to left node
             else if (N.value > theValue)
                 InsertRecursive(ref N.left, theValue);
 
@@ -85,40 +118,53 @@ namespace BinarySearchTree
             else
                 InsertRecursive(ref N.right, theValue);
 
-            // Balance the Tree
-            int balance = height(N.left) - height(N.right);
-            if (balance > 1)
-            {
-                // LL
-                if (height(N.left.left) >= height(N.left.right))
-                    RotateRight(ref N);
-                // LR
-                else
-                {
-                    RotateLeft(ref N.left);
-                    RotateRight(ref N);
-                }
-            }
-            else if (balance < -1)
-            {
-                // RR
-                if (height(N.right.right) >= height(N.right.left))
-                    RotateLeft(ref N);
-                // RL
-                else
-                {
-                    RotateRight(ref N.right);
-                    RotateLeft(ref N);
-                }
-            }
-            else
-                // Set the new height
-                setHeight(ref N);
-
-
+            // Balance the tree
+            Balance(ref N);
+            
         }
 
-        // Print In Order (N = current Node)
+        // Delete the Node containing the specified data
+        public Node Delete(ref Node N, int theValue)
+        {
+            // if Current Node is null
+            if (N == null) return N;
+
+            // else iterate down (left or right)
+            else if (theValue < N.value) N.left = Delete(ref N.left, theValue);
+            else if (theValue > N.value) N.right = Delete(ref N.right, theValue);
+
+            // else Node has been found
+            else
+            {
+                // Case 1: No Children
+                if (N.left == null && N.right == null)
+                    N = null;
+
+                // Case 2: One Child
+                else if (N.left == null)
+                    N = N.right;
+                else if (N.right == null)
+                    N = N.left;
+
+                // Case 3: Two Children
+                else
+                {
+                    Node temp = FindMin(N.right);
+                    N.value = temp.value;
+                    N.right = Delete(ref N.right, temp.value);
+                }
+            }
+            Balance(ref N); // Set the new height
+            return N;
+        }
+
+        // Find the Node with the lowest value
+        public Node FindMin(Node N)
+        {
+            return (N.left == null) ? N : FindMin(N.left);
+        }
+
+        // Returns a string with the printed Tree [INORDER] (N = current Node)
         public string PrintInOrder(Node N)
         {
             if (N == null)
@@ -129,15 +175,15 @@ namespace BinarySearchTree
                 PrintInOrder(N.right);
         }
 
-        // Print In Order (N = current Node)
+        // Returns a string with the printed Tree [PREORDER] (N = current Node)
         public string PrintPreOrder(Node N)
         {
             if (N == null)
                 return "";
             return
                 N.value + ":" + N.height + " " +
-                PrintInOrder(N.left) +
-                PrintInOrder(N.right);
+                PrintPreOrder(N.left) +
+                PrintPreOrder(N.right);
         }
 
     }
--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -11,6 +11,64 @@ namespace BinarySearchTree
         static void Main(string[] args)
         {
 
+            // Create Tree
+            AVL_Tree myTree = new AVL_Tree();
+
+            // Print Header
+            Console.WriteLine("AVL Tree");
+
+            int selection = 0;
+
+            do
+            {
+                Console.WriteLine();
+                Console.WriteLine("[1] Insert multiple Random Numbers");
+                Console.WriteLine("[2] Insert an Item");
+                Console.WriteLine("[3] Delete an Item");
+                Console.WriteLine("[4] Print Tree Pre-Order");
+                Console.WriteLine("[5] Print Tree In-Order");
+                Console.WriteLine("[6] Reset Tree");
+                Console.WriteLine("[7] Quit Application");
+                Console.Write("Enter Selection: ");
+                selection = Convert.ToInt32(Console.ReadLine());
+
+                switch (selection)
+                {
+                    case 1:
+                        Console.Write("Enter the amount of random numbers to populate the tree with: ");
+                        myTree.InsertRandomNums(Convert.ToInt32(Console.ReadLine()));
+                        break;
+                    case 2:
+                        Console.Write("Enter the value that you want to insert: ");
+                        myTree.Insert(Convert.ToInt32(Console.ReadLine()));
+                        break;
+                    case 3:
+                        Console.Write("Enter the value of the item you want to delete: ");
+                        myTree.Delete(ref myTree.root, Convert.ToInt32(Console.ReadLine()));
+                        break;
+                    case 4:
+                        Console.WriteLine("\n" + myTree.PrintPreOrder(myTree.root));
+                        break;
+                    case 5:
+                        Console.WriteLine("\n" + myTree.PrintInOrder(myTree.root));
+                        break;
+                    case 6:
+                        myTree = new AVL_Tree();
+                        break;
+                    case 7:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a value from 1 - 6...");
+                        break;
+                }
+
+            } while (selection != 7) ;
+
+
+
+            /*
+
             // Create and populate tree
             AVL_Tree myTree = new AVL_Tree();
             myTree.InsertRandomNums(10);
@@ -25,6 +83,8 @@ namespace BinarySearchTree
             // Print results
             Console.WriteLine(myTree.PrintPreOrder(myTree.root));
             Console.ReadLine();
+
+            */
 
         }
     }
